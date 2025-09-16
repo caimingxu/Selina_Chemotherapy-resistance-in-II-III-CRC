e2d6c8cca82b024cb347cb4449b9a63e550d0af7
caimingxu <94650613+caimingxu@users.noreply.github.com>
2025-09-16 15:35:58 -0700

    Create Decision curve.R

---
+++ 2_Training&Validation/Decision curve.R
@@ -0,0 +1,52 @@
+
+library(devtools)
+#install_github("mdbrown/DecisionCurve")
+library(rmda) 
+library(export)
+# 5 years
+rt=read.table("5 year.txt",sep="\t",header=T,row.names=1,check.names=F) 
+
+model1 <- decision_curve(fustat ~ risk + N + Chemotherapy,
+                         data = rt,
+                         thresholds = seq(0, 1, by = .005),
+                         bootstraps = 10)
+model2<- decision_curve(fustat ~  risk ,
+                       data = rt,
+                       thresholds = seq(0, 1, by = .005),
+                        bootstraps = 10)
+plot
+plot_decision_curve(list(model1, model2),confidence.intervals=FALSE,
+                   curve.names = c("Nomogram", "Stage"),
+                   col = c("cornflowerblue","cornflowerblue"),
+                   cost.benefit.axis=FALSE,
+                   lwd = 2:2,
+                   lty = 1:2,
+                    standardize=FALSE,
+                    legend.position = "none")
+graph2ppt(file="5 years.pptx", width=10, height=6)
+
+# 2 years
+rt=read.table("2 year.txt",sep="\t",header=T,row.names=1,check.names=F) 
+
+model1 <- decision_curve(fustat ~  risk + N + Chemotherapy,
+                         data = rt,
+                         thresholds = seq(0, 1, by = .005),
+                         bootstraps = 10)
+model2<- decision_curve(fustat ~  risk ,
+                        data = rt,
+                        thresholds = seq(0, 1, by = .005),
+                        bootstraps = 10)
+plot
+plot_decision_curve(list(model1, model2),confidence.intervals=FALSE,
+                    curve.names = c("Nomogram", "Stage"),
+                    col = c("lightcoral", "lightcoral"),cost.benefit.axis=FALSE,
+                    lwd = 2:2,
+                    lty = 1:2,
+                    standardize=FALSE,
+                    legend.position = "none")
+graph2ppt(file="2 years.pptx", width=10, height=6)
+
+
+
+
+
